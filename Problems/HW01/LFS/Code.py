557caa7cc6a1d96b6a4fc61a1143a929c7ae8941
Mohsen Hosseini <mhosseini1400@gmail.com>
2025-02-15 13:43:57 +0330

    Q2

--- Problems/HW01/LFS/Code.py
+++ Problems/HW01/LFS/Code.py
@@ -4,6 +4,8 @@
 import lfsir
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+
 
 #%%
 
@@ -46,11 +48,13 @@ d= d.rename({'F2_D04': 'Gender'}, axis= 1)
 d['F2_D07'] = pd.to_numeric(d['F2_D07'], errors='coerce')
 d= d.rename({'F2_D07': 'Age'}, axis= 1)
 
+#%%
+
 d['ActivityStatus']= d['ActivityStatus'].map({
     1: 'Employed',
     2: 'Unemployed',
     3: 'Inactive'
-    }).fillna('Below15')
+    }).fillna('Below10')
 
 #%%
 
@@ -73,21 +77,23 @@ d= d.rename({'F2_D17': 'EducationLevel'}, axis= 1)
 
 # we consider those above 65 to be out of working age
 d['ActivityStatusNew'] = np.where(d['Age'] >= 65, 'Over65', d['ActivityStatus'])
+d['ActivityStatusNew'] = np.where(d['Age'] < 15, 'Below15', d['ActivityStatusNew'])
+
+
+d['Active']= np.where(
+    d['ActivityStatus'].isin(['Employed', 'Unemployed']), 
+    d['IW_Yearly'], 
+    0)
+
+d['Employed']= np.where(
+    d['ActivityStatus'].isin(['Employed']), 
+    d['IW_Yearly'], 
+    0)
 
 #%%
 
 # filtering to those in working age
 d_q= d[d['ActivityStatusNew'].isin(['Inactive', 'Employed', 'Unemployed'])]
-
-d_q['Active']= np.where(
-    d_q['ActivityStatusNew'].isin(['Employed', 'Unemployed']), 
-    d_q['IW_Yearly'], 
-    0)
-
-d_q['Employed']= np.where(
-    d_q['ActivityStatusNew'].isin(['Employed']), 
-    d_q['IW_Yearly'], 
-    0)
 
 d_q= d_q.groupby(
     ['Gender', 'EducationLevel'], 
@@ -109,18 +115,47 @@ d_q= d_q.pivot(
     values= ['ParticipationRate', 'UnemploymentRate']
     ).reset_index()
 
+#%%
+
 d_q.to_excel('Q1.xlsx')
+
+#%%
+
+# 2 - Distribution of Unemployment Rate and Employmentto-Population Ratio by Age
+d_q= d[d['ActivityStatusNew']!= 'Below15']
+
+d_q= d_q.groupby(
+    ['Age'], 
+    as_index= False)\
+    .agg({
+        'IW_Yearly': 'sum',
+        'Active': 'sum',
+        'Employed': 'sum'
+        })
+    
+d_q['UnemploymentRate']= 100* (1- d_q['Employed']/ d_q['Active']).fillna(0)
+d_q['EmploymentRate']= 100* (d_q['Employed']/ d_q['IW_Yearly']).fillna(0)
 
 
 #%%
 
+plt.figure(figsize=(8, 5))
 
+# Plot lines with colors and line styles
+plt.plot(d_q['Age'], d_q['UnemploymentRate'], label='Unemployment Rate', color='red', linestyle='--', linewidth=2)
+plt.plot(d_q['Age'], d_q['EmploymentRate'], label='Employment-Population Ratio', color='blue', linewidth=2)
 
+plt.legend(frameon=False)
+plt.title('Unemployment & Employment Ratio by Age', fontsize=12, fontweight='bold')
+plt.xlabel('Age', fontsize=10)
+plt.ylabel('Rate (%)', fontsize=10)
+plt.xticks(fontsize=9)
+plt.yticks(fontsize=9)
+plt.grid(True, linestyle=':', alpha=0.5)
 
+plt.show()
 
-
-
-
+#%%
 
 
 
