20ac34de050c2d3090bd39e9f582284877224a24
Mohsen Hosseini <mhosseini1400@gmail.com>
2025-02-15 19:24:45 +0330

    q5 edit

--- Problems/HW01/HEIS/CodeHEIS.py
+++ Problems/HW01/HEIS/CodeHEIS.py
@@ -6,10 +6,13 @@ import pandas as pd
 #%%
 
 d1= pd.read_excel('SumR1401.xlsx')
+d1['Type']= 'Rural'
 d2= pd.read_excel('SumU1401.xlsx')
+d2['Type']= 'Urban'
 
 d= pd.concat([d1, d2])
 cols= d.columns
+
 #%%
 
 # 5 - Summary Statistics for Household Income and Education
@@ -23,8 +26,27 @@ d= d.rename({
      },
     axis= 1)
 
-d_q= d[['HeadEducationLevel', 'Income', 'HeadCount']].describe()
-d_q.to_excel('Q5.xlsx', index= False)
+#%%
+
+# add province
+d['ProvinceCode']= d['ADDRESS'].astype('str').str[1:3].astype(int)
+prov= pd.read_excel('ProvinceMap.xlsx')
+
+d= d.merge(prov, on= 'ProvinceCode')
+
+#%%
+
+d_q= d.groupby(
+    ['Type', 'Province'], 
+    as_index= False)\
+    .agg({
+        'HeadEducationLevel': ['min', 'max', 'mean', 'std', 'count'],
+        'Income': ['min', 'max', 'mean', 'std', 'count'],
+        'HeadCount': ['min', 'max', 'mean', 'std', 'count']
+        })
+
+
+d_q.to_excel('Q5.xlsx')
 
 #%%
 
