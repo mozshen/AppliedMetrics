9b20385cfacef7ae6f6c97dd81af0993285fe67f
Mohsen Hosseini <mhosseini1400@gmail.com>
2025-02-15 12:12:49 +0330

    create requeired columns

--- Problems/HW01/LFS/Code.py
+++ Problems/HW01/LFS/Code.py
@@ -3,6 +3,7 @@
 
 import lfsir
 import pandas as pd
+import numpy as np
 
 #%%
 
@@ -31,24 +32,48 @@ d_cleaned= lfsir.load_table(
 
 #%%
 
-
 # 1 - Labor Force Participation and Unemployment Rates
-# we define labour force partitcipation those between 15 and 65 year
-# who are currently working or try to find work
+
+# editing gender
+d['F2_D04']= d['F2_D04'].map({1: 'Male', 2: 'Female'})
+d= d.rename({'F2_D04': 'Gender'}, axis= 1)
+
+# editing age
+d['F2_D07'] = pd.to_numeric(d['F2_D07'], errors='coerce')
+d= d.rename({'F2_D07': 'Age'}, axis= 1)
+
+d['ActivityStatus']= d['ActivityStatus'].map({
+    1: 'Employed',
+    2: 'Unemployed',
+    3: 'Inactive'
+    }).fillna('Below15')
+
+# editing education
+d['F2_D17']= d['F2_D17'].map({
+    1: 'Below High School and High School',
+    2: 'Below High School and High School',
+    3: 'Below High School and High School',
+    4: 'Below High School and High School',
+    5: 'Below High School and High School',
+    6: 'Bachelor’s Degree',
+    7: 'Master’s and Higher',
+    8: 'Master’s and Higher',
+    9: 'Other'
+    }).fillna('Non-Literate')
+
+d= d.rename({'F2_D17': 'EducationLevel'}, axis= 1)
 
 
+# we consider those above 65 to be out of working age
+d['ActivityStatusNew'] = np.where(d['Age'] >= 65, 'Over65', d['ActivityStatus'])
 
+# filtering to those in working age
+d_q= d[d['ActivityStatusNew'].isin(['Inactive', 'Employed', 'Unemployed'])]
 
-
-
-
-
-
-
-
-
-
-
+d_q['Active']= d_q['ActivityStatusNew'].map({
+    'Employed': d_q['IW_Yearly'],
+    'Unemployed': d_q['IW_Yearly']
+    }).fillna(0)
 
 
 
