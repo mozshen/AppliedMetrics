3d248feee8fec95e9b60c03460978274d128ed61
Mohsen Hosseini <mhosseini1400@gmail.com>
2025-03-01 14:48:32 +0330

    q5

--- Problems/HW2/Q5.py
+++ Problems/HW2/Q5.py
@@ -23,5 +23,44 @@ plt.show()
 
 #%%
 
+# c. OLS regression
+# Define the independent variables: 
+d_X = d[['atndrte', 'frosh', 'soph']]
+d_X = sm.add_constant(d_X)
+
+# Define the dependent variable
+d_Y = d['stndfnl']
+
+# Run the OLS regression
+model = sm.OLS(d_Y, d_X).fit()
+
+# Print the regression results
+print(model.summary())
+
+#%%
+
+# residuals histogram
+plt.figure(figsize=(8, 5))
+plt.hist(model.resid, bins=30, edgecolor='black', alpha=0.7, color='skyblue', density=True)
+plt.axvline(model.resid.mean(), color='red', linestyle='dashed', linewidth=2, label='Mean')
+plt.xlabel('Residuals')
+plt.ylabel('Density')
+plt.title('Distribution of Residuals')
+plt.grid(axis='y', linestyle='--', alpha=0.7)
+plt.legend()
+plt.show()
+
+#%%
+
+# resuduals and target
+plt.figure(figsize=(8, 5))
+plt.scatter(d['stndfnl'], model.resid, alpha=0.5, color='blue', edgecolor='black')
+plt.axhline(0, color='red', linestyle='dashed', linewidth=2)
+plt.xlabel('stndfnl')
+plt.ylabel('Residuals')
+plt.title('Residuals vs. stndfnl')
+plt.grid(True, linestyle='--', alpha=0.7)
+plt.show()
 
 
+#%%
